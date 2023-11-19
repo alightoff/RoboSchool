db43300bcb6fd6176d7fb2b8eeda49d898e9c50d
Egor <142628503+alightoff@users.noreply.github.com>
2023-11-19 18:04:32 +0300

    Add files via upload

---
+++ лендинг школы ROBO SCHOOL/js/main.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 30,
+  centeredSlides: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+    dynamicBullets: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var appendNumber = 4;
+var prependNumber = 1;
+document
+  .querySelector(".prepend-2-slides")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.prependSlide([
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+    ]);
+  });
+document
+  .querySelector(".prepend-slide")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.prependSlide(
+      '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+    );
+  });
+document
+  .querySelector(".append-slide")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.appendSlide(
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+    );
+  });
+document
+  .querySelector(".append-2-slides")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    swiper.appendSlide([
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+      '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+    ]);
+  });
